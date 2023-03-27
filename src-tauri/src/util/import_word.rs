b31018c6e825922d72fd3dfd72befed031e78b69
chencong <sccong@outlook.com>
2023-03-27 15:35:09 +0800

    todo

--- src-tauri/src/util/import_word.rs
+++ src-tauri/src/util/import_word.rs
@@ -44,48 +44,35 @@ pub async fn upload_word(
                 header::HeaderValue::from_str(token).unwrap(),
             );
 
-            // let mut future_vec = Vec::new();
-            // word_vec.push("feast".to_string());
-            let len = words.len();
-            // if len < 100 {
-            //     let param = serde_json::json!({
-            //         "business_id": 6,
-            //         "words": words,
-            //     });
-            //     let result_response = client.post(url).headers(headers).json(&param).send().await;
-            // } else {
-            //     for chunk in words.chunks(100) {
-            //         let param = serde_json::json!({
-            //             "business_id": 6,
-            //             "words": chunk,
-            //         });
-            //         let result_response =
-            //             client.post(url).headers(headers).json(&param).send().await;
-            //     }
-            // }
             let mut future_vec = Vec::new();
-            let param = serde_json::json!({
-                "business_id": 6,
-                "words": words,
-            });
-            let result_response = client.post(url).headers(headers).json(&param).send();
-            future_vec.push(spawn(result_response));
-
+            let len = words.len();
+            if len < 100 {
+                let param = serde_json::json!({
+                    "business_id": 6,
+                    "words": words,
+                });
+                let result = client
+                    .post(url)
+                    .headers(headers.to_owned())
+                    .json(&param)
+                    .send();
+                future_vec.push(result);
+            } else {
+                for chunk in words.chunks(100) {
+                    let param = serde_json::json!({
+                        "business_id": 6,
+                        "words": chunk,
+                    });
+                    let result = client
+                        .post(url)
+                        .headers(headers.to_owned())
+                        .json(&param)
+                        .send();
+                    future_vec.push(result);
+                }
+            };
             join_all(future_vec).await;
             Ok(String::from("success"))
-            // match res {
-            //     Ok(response) => {
-            //         let json_res = response.json::<ApiResponse>().await;
-            //         match json_res {
-            //             Ok(res) => {
-            //                 println!("{:?}", res);
-            //                 Ok(res.task_id)
-            //             }
-            //             Err(e) => Err(e.to_string()),
-            //         }
-            //     }
-            //     Err(e) => Err(e.to_string()),
-            // }
         }
         Err(err) => Err(err.to_string()),
     }
